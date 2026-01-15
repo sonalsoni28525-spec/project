e55ca116e9a23abf85b1b4ec2422388f1cd1bb0e
Byte Buddies <sonalsoni28525@gmail.com>
2026-01-15 11:22:58 +0530

    Update user .js

--- java/user .js
+++ java/user .js
@@ -1,1 +1,27 @@
-document .getElement
+document .getElementbyid("userform").addEventlistener("register",asyn(e) => {
+e.preventDefaut();
+   constname= document.getElementbyid ("Name").value .trim();
+            constemail= document.getElementbyid ("Email").value .trim();
+  constpassword= document.getElementbyid ("Password").value .trim();
+   if(!Name ||!Email  ||!Password) {
+alert(" All fields are required");
+     return;
+   }
+  const{ data: exitinguser, error: checkError } =
+    await superbaseClient
+  .form("users")
+  .select("id")
+  .eq("Email",Email)
+  .maybesingle();
+  if(checkError) {
+    console.error(checkError);
+    alert("Error checking user");
+    retutn;
+  }
+  if(exitingUser)
+  {
+    alert("Email already Registered")
+    return;
+  }
+  const{ error:insertError}=await 
+    
