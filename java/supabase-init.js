7a67f4102c147f836da2008f328c93b177f1b0fd
Byte Buddies <sonalsoni28525@gmail.com>
2026-01-31 12:41:42 +0530

    Update supabase-init.js

--- java/supabase-init.js
+++ java/supabase-init.js
@@ -1,1 +1,11 @@
-
+const supabase Client= window.supabase.createClient(
+  "https://muhblwgrpndptbjyatlr.supabase.co"
+  "sb_publishable_XCSzLvd_E5fw35ZJ7xvJSA_XZVXG_ZR"
+);
+ if (supabaseClient) {
+   
+console.log("databse Connected:", supabaseClient);
+ }
+else{
+  console.log("database not connected");
+}
